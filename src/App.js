eae84361c445017e81ab82fc35bf581038019f2f
Antwone <antwone@gmail.com>
2019-03-19 15:08:46 -0400

    working with filtering

--- src/App.js
+++ src/App.js
@@ -13,26 +13,32 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {value: '', data: [] };
-
+        this.state = { data: [], min_stars: 0, max_stars: 20000, max_issues:20000 };
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event) {
         console.log('target: ' + event.target );
         console.log('name: ' + event.target.name);
+        const name = event.target.name;
+        const value = event.target.value;
         let fetchPromise = null;
-        if(event.target.name === 'org_name'){
-            fetchPromise = fetchRepos(event.target.value );
+        if(name === 'org_name'){
+            fetchPromise = fetchRepos(value);
         }
-        if(event.target.name === 'min_star'){
-            fetchPromise = fetchRepos(event.target.value );
+        let newState = null;
+        if(name === 'min_star'){
+            newState = {...this.state, min_stars: value};
         }
-        if(event.target.name === 'max_star'){
-            fetchPromise = fetchRepos(event.target.value );
+        if(name === 'max_star'){
+            newState = {...this.state, max_stars: value};
         }
-        if(event.target.name === 'max_issues'){
-            fetchPromise = fetchRepos(event.target.value );
+        if(name === 'max_issues'){
+            newState = {...this.state, max_issues: value};
+        }
+        if(newState)
+        {
+            this.setState(newState)
         }
         if(fetchPromise !== null)
         {
@@ -40,7 +46,7 @@ class App extends Component {
                 daReturn => {
                     console.log("****> Promise Just Came Back");
                     console.log("****> daReturn: " + daReturn.length);
-                    this.setState( {value: '', data: daReturn } );
+                    this.setState( { data: daReturn } );
                 }
             );
         }
@@ -69,6 +75,35 @@ class App extends Component {
     render() {
         console.log("----REDRAWING-----");
         const jsonString = this.state.data ? JSON.stringify(this.state.data) : "No Data";
+        /// {"name":"cn.github.io","issues":9,"watchers":57,"stars":57},
+        const filteredData = this.state.data.filter( dataObj => {
+            let filterOut = true;
+            if(this.state.min_stars > 0)
+            {
+                if(dataObj.stars < this.state.min_stars)
+                {
+                    console.log(`Filtering out b/c min_stars ${this.state.min_stars} < stars ${dataObj.stars}`);
+                    filterOut = false;
+                }
+            }
+            if(this.state.max_stars)
+            {
+                if(dataObj.stars > this.state.max_stars)
+                {
+                    console.log(`Filtering out b/c max_stars ${this.state.max_stars} > stars ${dataObj.stars}`);
+                    filterOut = false;
+                }
+            }
+            if(this.state.max_issues)
+            {
+                if(dataObj.issues > this.state.max_issues)
+                {
+                    console.log(`Filtering out b/c max_stars ${this.state.max_issues} > stars ${dataObj.stars}`);
+                    filterOut = false;
+                }
+            }
+            return filterOut;
+        })
         return (
             <div className="App">
                 <form>
@@ -79,20 +114,28 @@ class App extends Component {
                     <br/>
                     <label>
                         Min Star:
-                        <input type="text" name="min_star" />
+                        <input type="text" name="min_star" onChange={this.handleChange} />
                     </label>
                     <br/>
                     <label>
                         Max Stars:
-                        <input type="text" name="max_star" />
+                        <input type="text" name="max_star"  onChange={this.handleChange}/>
                     </label>
                     <br/>
                     <label>
                         Max Issues:
-                        <input type="text" name="max_issues" />
+                        <input type="text" name="max_issues"  onChange={this.handleChange}/>
                     </label>
                 </form>
-                <DataViewer name={'ANTWONE_WALTERS'}/>
+                Result Size: {filteredData.length}
+                {
+                    filteredData.map( dataObj =>
+                    <div>
+                        <DataViewer name={dataObj.name}/>
+                        {JSON.stringify(dataObj)}
+                    </div> )
+                }
+                <hr/>
                 {jsonString}
             </div>
         );
--- src/DataViewer.js
+++ src/DataViewer.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class DataViewer extends React.Component {
     render() {
         return <h1>Hello, {this.props.name}</h1>;
     }
 }
+
+DataViewer.defaultProps = {
+    name: 'Optimus_Prime'
+};
+
+DataViewer.propTypes = {
+    name: PropTypes.string
+};
 
 export default DataViewer;
